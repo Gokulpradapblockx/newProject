d51c1788f88cc7ed9951f1f4de723896c49be39f
Gokulpradap <gokulmari96@gmail.com>
2022-03-21 15:24:31 +0530

    all

--- src/Servicecard/Expertise.jsx
+++ src/Servicecard/Expertise.jsx
@@ -1,61 +1,55 @@
-import { Form } from 'formik'
-import React from 'react'
-import web from '../Assets/web.svg';
-import mobile from '../Assets/mobiles.svg'
-import cms from '../Assets/cms.svg';
-import graphic from '../Assets/graphic-tool.svg'
-import research from '../Assets/research.svg';
-
-
+import { Form } from "formik";
+import React from "react";
+import web from "../Assets/web.svg";
+import mobile from "../Assets/mobiles.svg";
+import cms from "../Assets/cms.svg";
+import graphic from "../Assets/graphic-tool.svg";
+import research from "../Assets/research.svg";
 
 const Expertise = () => {
   return (
-   <>
-   <div className="srv-card">
-       <div className="row space">
-           <div className="col-2">
+    <>
+      <div className="srv-card">
+        <div className="row space">
+          <div className="col-2">
             <div className="card-size">
-                <img src="" alt="" />
-                <img src= {web} alt="" />
-                <h5>Web Developement</h5>
+              <img src="" alt="" />
+              <img src={web} alt="" />
+              <h5>Web Developement</h5>
             </div>
-           </div>
-           <div className="col-2">
-           <div className="card-size">
-                <img src="" alt="" />
-                <img src={mobile} alt="" />
-                <h5>Mobile
-Development</h5>
-            </div>
-           </div>
-           <div className="col-2">
-           <div className="card-size">
-                <img src="" alt="" />
-                <img src={research} alt="" />
-                <h5>Quality
-Assurance</h5>
-            </div>
-           </div>
-           <div className="col-2">
+          </div>
+          <div className="col-2">
             <div className="card-size">
-                <img src="" alt="" />
-                <img src={graphic} alt="" />
-                <h5>UI / UX
-Design</h5>
+              <img src="" alt="" />
+              <img src={mobile} alt="" />
+              <h5>Mobile Development</h5>
             </div>
-           </div>
-           <div className="col-2">
-           <div className="card-size">
-                <img src="" alt="" />
-                <img src= {cms} alt="" />
-                <h5>DevOps
-Services</h5>
+          </div>
+          <div className="col-2">
+            <div className="card-size">
+              <img src="" alt="" />
+              <img src={research} alt="" />
+              <h5>Quality Assurance</h5>
             </div>
-           </div>
-       </div>
-   </div>
-   </>
-  )
-}
+          </div>
+          <div className="col-2">
+            <div className="card-size">
+              <img src="" alt="" />
+              <img src={graphic} alt="" />
+              <h5>UI / UX Design</h5>
+            </div>
+          </div>
+          <div className="col-2">
+            <div className="card-size">
+              <img src="" alt="" />
+              <img src={cms} alt="" />
+              <h5>DevOps Services</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Expertise;
